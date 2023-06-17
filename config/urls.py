010d7cc08c115c54f14b4ab1ead3cb2fff1277bf
root <plusf2p@gmail.com>
2023-06-17 11:25:59 +0000

    lesson_2 Hello world with CBV and FBV

--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.HelloWorldView.as_view()),
+    path("<str:word>/", views.check_kwargs),
 ]
